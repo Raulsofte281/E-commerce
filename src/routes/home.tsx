51696124072e7ce26e1e5e7125fc29311eb663d6
Raul <rauldondonimachado09@gmail.com>
2025-10-14 17:37:56 -0300

    Download banner

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { Header } from "./header";
+import banner from '../assets/banner.svg'
 
 export function Home(){
     return (
         <>
-        <div className="w-full flex justify-center bg-[#FFFFFF]">
+        <div className="w-full bg-[#FFFFFF]">
             <Header/>
-            <div>
-                
+            <div className="my-6 w-full">
+                <img src={banner} className="w-full"/>
             </div>
         </div>
         </>
